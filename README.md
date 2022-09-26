0ecc704981eb91787c060f37def13a71f11dc3fa
Lars-Peter Meyer <lpmeyer@infai.org>
2022-09-26 21:49:24 +0200

    added command line parameter parsing and improved script
    added command line parameter handling with getopt from unix-utils
    
    added some bash options for error handling and comments
    
    updated README accordingly
    
    added verbose flag to RMLmapper call if debug flag is given

--- README.md
+++ README.md
@@ -8,9 +8,40 @@ The spreadsheet can easily get translated to a turtle coded owl file with the YA
 
 ## Usage
 
+usage information as printed with `--help` option:
+```
+usage:
+xlsx2owl-StahlDigital.sh [options] [download-url]
+
+positional parameters:
+  [download-url]:
+            optional url from where the spreadsheet gets downloaded.
+            Take local input file if no download-url is given.
+            needs to start with a protocoll (e.g. https://...),
+            supported protocolls are http, https and file.
+named parameters:
+  -h, --help:
+            produces this usage info and exits.
+  -d, --debug :
+            enable debug output
+  -y, --yarrrml <FILE> :
+            path <FILE> to the yarrrml mapping file to use.
+            defaults to 'yarrrml.yml' in the scripts directory.
+  -o, --outputPrefix <PREFIX> :
+            set prefix <PREFIX> for the generated output.
+            default 'xlst-out', relativ to the current working directory.
+  -i, --input <FILE> :
+            path <FILE> to the input spreadsheet file to use
+            or where to store spreadfile downloaded.
+            default 'xlsx2owl-tmp.xlsx'
+```
+
+### run as docker container
 * build docker image, e.g. via `$ buildah bud -t xlsx2owl-sd ./`
 * switch to working directory
-* run docker image, e.g. via `$ podman run -it --rm -v "./:/home/user/data" xlsx2owl-sd 'file:///home/user/xlsx2owl-Example.xlsx'`
+* run docker image, e.g. via `$ podman run -it --rm -v "./:/data/" xlsx2owl-sd --input '/home/user/xlsx2owl-Example.xlsx'`
+* run docker image with test input e.g. via `$ podman run -it --rm -v "./test:/data/" xlsx2owl-sd:test --debug -o test-o
+ut 'file:///data/test-input.xlsx'`
 
 ## Dependencies
 
@@ -18,9 +49,10 @@ The spreadsheet can easily get translated to a turtle coded owl file with the YA
 * Java 1.8 (from RML)
 * node.js 14 (from YARRRML)
 * bash >=5 (maybe from bash regex)
+* getopt from unix-utils
 
 ## Acknowledgement
-
+used tools:
 * YARRRML
 * RML
 * xlsx2csv
--- xlsx2owl-StahlDigital.sh
+++ xlsx2owl-StahlDigital.sh
@@ -1,4 +1,4 @@
-#\bin\bash
+#!/bin/bash
 
 ###
 # Requirements:
@@ -12,6 +12,22 @@
 ###
 
 ###
+# init bash options
+###
+set -o nounset # print out error on usage of unset variable
+set -o errexit # exit on any error
+# as errexit makes it difficult to get result code from failing commands, use e.g. "cat /tmp/doesnotexist && rc=$? || rc=$?"
+# see https://stackoverflow.com/questions/9629710/what-is-the-proper-way-to-detect-shell-exit-code-when-errexit-option-is-set#15844901
+set -o pipefail # exit on failed pipe commands
+
+
+############
+############
+## initialization and parameter parsing
+############
+############
+
+###
 # define some variables
 ###
 # directory that should contain a tool directory
@@ -19,49 +35,143 @@ optScriptDir="$(dirname "${0}")"
 # work directory
 optWorkFolder="./"
 # tool directory
-optToolFolder=${optScriptDir}/tools
+optToolFolder="${optScriptDir}/tools"
 # resources directory
-optResourcesFolder=${optScriptDir}/resources
+optResourcesFolder="${optScriptDir}/resources"
 # input xlsx filename, used as download target filename as well if download specified
-optXlsxFilename="xlsx2owl-StahlDigital.xlsx"
+optXlsxFilename="xlsx2owl-tmp.xlsx"
 # YARRRML filename
 optYarrrmlFilename="yarrrml.yml"
 # csv tmp folder
 optCsvFolder="${optWorkFolder}csv"
 # output Prefix
-optOutputFilenamePrefix="StahlDigital-vocab"
+optOutputFilenamePrefix="xlst-out"
 # url for downloading spreadsheet
 unset optDownloadUrl
-optDebug=true
+optDebug=false
+
 
 ###
-# parse options
+# define options and help message
 ###
+
 printUsage() {
-    echo "'xlsx2owl-StahlDigital.sh [download-url-starting-with-http]'"
+cat <<EOF
+'xlsx2owl-StahlDigital.sh [options] [download-url]'
+positional parameters:
+  [download-url]:
+            optional url from where the spreadsheet gets downloaded.
+            Take local input file if no download-url is given.
+            needs to start with a protocoll (e.g. https://...),
+            supported protocolls are http, https and file.
+named parameters:
+  -h, --help:
+            produces this usage info and exits.
+  -d, --debug :
+            enable debug output
+  -y, --yarrrml <FILE> :
+            path <FILE> to the yarrrml mapping file to use.
+            defaults to 'yarrrml.yml' in the scripts directory.
+  -o, --outputPrefix <PREFIX> :
+            set prefix <PREFIX> for the generated output.
+            default 'xlst-out', relativ to the current working directory.
+  -i, --input <FILE> :
+            path <FILE> to the input spreadsheet file to use
+            or where to store spreadfile downloaded.
+            default 'xlsx2owl-tmp.xlsx'
+EOF
 }
 
+# added '+' at short option string to enable the positional parameter
+LONGOPTS=help,debug,yarrrml:,outputPrefix:,input:
+OPTIONS=+hdy:o:i:
+
+###
+# check a modern version of getopt from unix-utils is present
+###
+! getopt --test > /dev/null 
+if [[ ${PIPESTATUS[0]} -ne 4 ]]; then
+    echo '`getopt --test` failed in this environment.' >> /dev/stderr
+    exit 1
+fi
+
+
+###
+# prepare parameters using getopt from unix-util as suggested at https://stackoverflow.com/a/29754866
+###
+# use ! and $PIPESTATUS[0] because of "set -o errexit"
+! PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@")
+if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
+    # e.g. return value is 1
+    #  then getopt has complained about wrong arguments to stdout
+    echo "error parsing parameters" >> /dev/stderr
+    printUsage
+    exit 2
+fi
+eval set -- "$PARSED"
+
+###
+# iterate over named parameters passed
+while true; do
+    case "$1" in
+        -h|--help)
+            printUsage
+            exit 0
+            ;;
+        -d|--debug)
+            optDebug=true
+            shift
+            ;;
+        -y|--yarrrml)
+            optYarrrmlFullFilepath="$2"
+            shift 2
+            ;;
+        -o|--outputPrefix)
+            optOutputFilenamePrefix="$2"
+            shift 2
+            ;;
+        -i|--input)
+            optXlsxFilename="$2"
+            shift 2
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            echo "Programming error" >> /dev/stderr
+            exit 3
+            ;;
+    esac
+done
+
+###
+# define some more variables 
+###
+optYarrrmlFullFilepath="${optYarrrmlFullFilepath:=${optScriptDir}/${optYarrrmlFilename}}"
+optRmlFullFilepath="${optYarrrmlFullFilepath}.ttl"
+
+
+###
+# read in download url parameter if present
+###
 if [[ $# -eq 1 ]]
 then
-    ## checking url
+    ## checking url format
     ## should start with http or https or file and not contain one of { ";'}:
     if [[ "${1}" =~ ^(https?|file):\/\/[^\ \"\;\']+$ ]]
     then
         optDownloadUrl="${1}"
     else
-        echo "unexpected url format (file,http or https supported): '$*'"
+        echo "unexpected url format (file,http or https supported): '$*'" >> /dev/stderr
         printUsage
-        exit 1
+        exit 4
     fi
-    
-elif [[ $# -eq 0 ]]
+elif [[ $# -ne 0 ]]
 then
-    true
-    # using local spreadsheet
-else
-    echo "unexpected parameters: '$*'"
+    echo "to many parameters: '$*'" >> /dev/stderr
     printUsage
-    exit 1
+    exit 5
 fi
 
 ###
@@ -71,46 +181,75 @@ if [[ "${optDebug}" = true ]]
 then
     echo "variable values:"
     set | grep ^opt
+    echo
 fi
 
-###
-# process
-###
 
-## download if url given
-if [[ -n ${optDownloadUrl} ]]
+############
+############
+## download and prepare input files, run mapping
+############
+############
+
+###
+# download if url given
+###
+if [[ -n ${optDownloadUrl:-} ]] # expand variable because of "set -o nounset"
 then
 ## download file
     echo "downloading spreadsheet from '${1}'"
     curl "${1}" --output "${optXlsxFilename}"
 fi
 
-## create csv
+###
+# create csv input files
+###
 echo "converting xlsx to csv"
 python3 "${optToolFolder}/xlsx2csv.py" --all "${optXlsxFilename}" "${optCsvFolder}"
 
-## create rml from yarrrml
+
+###
+# create RML mapping file from yarrrml
+###
 echo "create rml rules from yarrrml"
 echo "  yarrrml version $(~/node_modules/@rmlio/yarrrml-parser/bin/parser.js --version || true)"
 ~/node_modules/@rmlio/yarrrml-parser/bin/parser.js --version
-~/node_modules/@rmlio/yarrrml-parser/bin/parser.js --pretty -i "${optScriptDir}/${optYarrrmlFilename}" -o "${optWorkFolder}/${optYarrrmlFilename}.ttl"
+~/node_modules/@rmlio/yarrrml-parser/bin/parser.js --pretty -i "${optYarrrmlFullFilepath}" -o "${optRmlFullFilepath}"
+RC=$?
+if [[ "${optDebug}" = true ]]
+then
+    echo "  RC=$RC"
+fi
 
-## create graph as turtle and nquads
+###
+# create graph as turtle and nquads
+###
 runMapper() {
-    paramType=${1}
-    paramSuffix=${2}
+    paramType="${1}"
+    paramSuffix="${2}"
     targetFilename="${optOutputFilenamePrefix}.${paramSuffix}"
+    
+    debugParameter=""
+    if [[ "${optDebug}" = true ]]
+    then
+        debugParameter="--verbose"
+    fi
     echo -n "  ${paramType}..."
-    java -jar -DIRI_PREFIX_MAP_FILE=${optCsvFolder}/prefixes.csv "${optToolFolder}/rmlmapper.jar" --mappingfile "${optWorkFolder}/${optYarrrmlFilename}.ttl" --duplicates --functionfile "${optResourcesFolder}/functions_xlsx2owl.ttl" --functionfile "${optResourcesFolder}/functions_grel.ttl" --functionfile "${optResourcesFolder}/grel_java_mapping.ttl" --serialization "${paramType}" --outputfile "${targetFilename}"
+    java -jar "-DIRI_PREFIX_MAP_FILE=${optCsvFolder}/prefixes.csv" "${optToolFolder}/rmlmapper.jar" --mappingfile "${optRmlFullFilepath}" --duplicates --functionfile "${optResourcesFolder}/functions_xlsx2owl.ttl" --functionfile "${optResourcesFolder}/functions_grel.ttl" --functionfile "${optResourcesFolder}/grel_java_mapping.ttl" --serialization "${paramType}" --outputfile "${targetFilename}" ${debugParameter}
+    RC=$?
+    if [[ "${optDebug}" = true ]]
+    then
+      echo -n "RC=$RC "
+    fi
     echo "${targetFilename}"
 }
 echo "create graph as turtle and nquads..."
 echo "rmlmapper version"; unzip -p "${optToolFolder}/rmlmapper.jar" "META-INF/maven/be.ugent.rml/rmlmapper/pom.properties" | grep -E "version=|artifactId="
 runMapper turtle ttl
 runMapper nquads nq
-# podman run --rm -v "$(pwd):/home/rmluser/data" yarrrmlmapper "${$optYarrrmlFilename}" --use-local-rml-mapper --duplicates --functionfile resources/functions_xlsx2owl.ttl --serialization turtle --outputfile "${optOutputFilenamePrefix}.ttl"
-#podman run --rm -v "$(pwd):/home/rmluser/data" yarrrmlmapper "${$optYarrrmlFilename}" --use-local-rml-mapper --duplicates --functionfile resources/functions_xlsx2owl.ttl --serialization nquads --outputfile "${optOutputFilenamePrefix}.nq"
 
-## count lines
-echo "triples( == nquad lines) generated:"
+###
+# output statistics (triples=lines in nquad serialization
+###
+echo -n "triples( == nquad lines) generated:"
 wc --lines "${optOutputFilenamePrefix}.nq"
