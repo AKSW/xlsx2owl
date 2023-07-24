0f711fa42a32fa827b4a757195de910c5da148d3
Lars-Peter Meyer <lpmeyer@infai.org>
2023-07-24 12:56:25 +0200

    fixed typo

--- xlsx2owl.sh
+++ xlsx2owl.sh
@@ -18,7 +18,7 @@
 # * yarrrml-parser at ~/node_modules/@rmlio/yarrrml-parser (e.g. via `npm i -g @rmlio/yarrrml-parser`)
 ###
 
-VERSION = "2.01"
+VERSION = "2.0"
 echo "xlsx2owl Version ${VERSION}"
 
 ###
